e9dc8debab9270d336445bf6f8454f0e674a3ec5
cmoralesd <claudio.morales@edutecnica.cl>
2024-07-28 11:54:41 -0400

    add files

---
+++ launch/view.launch.py
@@ -0,0 +1,50 @@
+import os
+import xacro
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
+from launch_ros.actions import Node
+
+
+
+
+def generate_launch_description():
+
+    # Launch arguments
+    use_sim_time = LaunchConfiguration('use_sim_time')
+
+    # Process the URDF file
+    pkg_path = os.path.join(get_package_share_directory('my_robot'))
+    xacro_file = os.path.join(pkg_path,'description/my_robot.xacro')
+    robot_description_config = xacro.process_file(xacro_file)
+
+    # Create a node for robot_state_publisher
+    params = {'robot_description': robot_description_config.toxml(), 'use_sim_time': use_sim_time}
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[params]
+    )
+
+    # Create a node for rviz2
+    rviz2_node = Node(
+        package='rviz2',
+        namespace='',
+        executable='rviz2',
+        name='rviz2',
+        arguments=['-d', [os.path.join(pkg_path, 'config', 'my_config.rviz')]]
+
+    )
+
+
+    # Launch!
+    return LaunchDescription([
+        DeclareLaunchArgument('use_sim_time', default_value='false',
+            description='Use simulation time if true'),
+
+        robot_state_publisher_node,
+        rviz2_node,
+    ])
---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+from glob import glob
+from setuptools import find_packages, setup
+
+package_name = 'my_robot'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share/' + package_name, 'launch'), glob(os.path.join('launch', '*launch.py'))),
+        (os.path.join('share/' + package_name, 'description'), glob(os.path.join('description', '*.*'))),
+        (os.path.join('share/' + package_name, 'description/urdf'), glob(os.path.join('description/urdf', '*.*'))),
+
+        (os.path.join('share/' + package_name, 'config'), glob(os.path.join('config', '*.*'))),
+
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='user',
+    maintainer_email='todo@todo.com',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
